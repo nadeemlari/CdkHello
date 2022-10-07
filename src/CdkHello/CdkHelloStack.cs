09d39909fc4a071c7d64511b6b774c665e8bf099
Mohammad Nadeem <nadeemlari@hotmail.com>
2022-10-07 12:58:49 +0530

    Initial commit

---
+++ src/CdkHello/CdkHelloStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace CdkHello
+{
+    public class CdkHelloStack : Stack
+    {
+        internal CdkHelloStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
